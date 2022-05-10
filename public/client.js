f2bd223acbd659f9975e24699c614dd8f87b9f7e
Nitin Srivastav <nitin.srivastav_cs19@gla.ac.in>
2022-05-10 14:57:55 +0530

    This is the initial commit

---
+++ public/client.js
@@ -0,0 +1,53 @@
+const socket=io()
+
+let name;
+let textarea=document.querySelector('#textarea')
+let messagearea=document.querySelector('.message_area')
+do{
+ name=prompt('please enter your name:')
+}while(!name)
+
+textarea.addEventListener('keyup' ,(e)=>{
+    if(e.key === 'Enter'){
+        sendMessage(e.target.value)
+    }
+})
+function sendMessage(message){
+    let msg={
+        user:name,
+        message:message.trim()
+    }
+    //Append message
+  appendMessage(msg,'outgoing')
+   
+  textarea.value="";
+  scrollToBottom()
+
+  //send to server
+
+  socket.emit('message' ,msg)
+}
+
+function appendMessage(msg,type){
+
+  let maindiv=document.createElement('div')
+  let className=type
+  maindiv.classList.add(className,'message')
+
+  let markup=`
+   <h4>${msg.user}</h4>
+   <p>${msg.message}</p>
+  `
+  maindiv.innerHTML=markup
+  messagearea.appendChild(maindiv)
+}
+
+//recieve message
+socket.on('message',(msg)=>{
+    appendMessage(msg,'incoming')
+    scrollToBottom()
+})
+
+function scrollToBottom(){
+    messagearea.scrollTop=messagearea.scrollHeight
+}
